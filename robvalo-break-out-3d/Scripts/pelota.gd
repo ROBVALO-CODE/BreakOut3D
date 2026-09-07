841dc3b574a4592cf8a7af672a6f71ea4ec17fa8
Gabriela <bautistalopezgabi@gmail.com>
2026-09-07 09:54:14 -0500

    integrar sistema de daño de bloques y señal block_destroyed

--- robvalo-break-out-3d/Scripts/pelota.gd
+++ robvalo-break-out-3d/Scripts/pelota.gd
@@ -3,13 +3,13 @@ class_name Pelota extends RigidBody3D
 signal block_destroyed
 
 enum GameState {Idle, Playing, GameOver}
-var state:GameState = GameState.Idle
+var state: GameState = GameState.Idle
 
-var moveDirection:Vector3
-var ballSpeed:float = 10.0
-var initvelocity:Vector3 = ballSpeed * Vector3.FORWARD
+var moveDirection: Vector3
+var ballSpeed: float = 10.0
+var initvelocity: Vector3 = ballSpeed * Vector3.FORWARD
 
-@onready var initPosition:Vector3 = position
+@onready var initPosition: Vector3 = position
 @onready var raqueta: Raqueta = $"../Raqueta"
 @onready var bloques: Node3D = $"../Bloques"
 @onready var nivel: Node3D = $".."
@@ -18,35 +18,36 @@ func _physics_process(delta: float) -> void:
 	match state:
 		GameState.Idle:
 			if Input.is_action_just_pressed("ui_accept"):
-				state =GameState.Playing
-				linear_velocity =initvelocity
+				state = GameState.Playing
+				linear_velocity = initvelocity
 		GameState.Playing:
 			moveDirection = linear_velocity.normalized()
-			linear_velocity =ballSpeed * moveDirection
-			angular_velocity =ballSpeed * Vector3.UP.cross(moveDirection)
+			linear_velocity = ballSpeed * moveDirection
+			angular_velocity = ballSpeed * Vector3.UP.cross(moveDirection)
 		GameState.GameOver:
-			linear_velocity =Vector3.ZERO
-			angular_velocity=Vector3.ZERO
+			linear_velocity = Vector3.ZERO
+			angular_velocity = Vector3.ZERO
 			
 func _integrate_forces(_state: PhysicsDirectBodyState3D) -> void:
 	if state == GameState.Idle:
 		position = initPosition
-		position.x =raqueta.position.x
-		
-
+		position.x = raqueta.position.x
 
 func _on_body_entered(body: Node) -> void:
 	if body is Block:
-		body.queue_free()
-		block_destroyed.emit() #Emite la señal para avisar que se ha destruido un bloque.
-		if bloques.get_child_count()== 1:
-			state = GameState.GameOver
-	
-
+		var block := body as Block
+		var vida_restante: float = block.recibir_dano(1.0)
+		
+		# Si el bloque se quedó sin vida, se destruye y se emite la señal
+		if vida_restante <= 0.0:
+			block.queue_free()
+			block_destroyed.emit()
+			
+			if bloques.get_child_count() == 1:
+				state = GameState.GameOver
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 	nivel.disparar_explosion(global_position)
-	state =GameState.Idle
-	linear_velocity =Vector3.ZERO
-	angular_velocity =Vector3.ZERO
-	
+	state = GameState.Idle
+	linear_velocity = Vector3.ZERO
+	angular_velocity = Vector3.ZERO
