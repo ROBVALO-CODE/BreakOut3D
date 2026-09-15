567ac299b44f1eb95f78f0f8c24da3099b95e206
RUB1C <metrodeivjuegos@gmail.com>
2026-09-15 05:57:02 -0500

    fix: Cambio de ruta en menú principal
    cambie una cosa que faltaba, la ruta para que desde el menú principal se pudiera acceder a Power-Ups

--- robvalo-break-out-3d/Scripts/menu_principal.gd
+++ robvalo-break-out-3d/Scripts/menu_principal.gd
@@ -11,7 +11,7 @@ func _on_how_to_play_button_pressed() -> void:
 
 
 func _on_power_ups_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/UI/power_ups.tscn")
 
 
 func _on_settings_button_pressed() -> void:
