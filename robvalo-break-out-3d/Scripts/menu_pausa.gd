f4dd0185ac18ab6bc46faf9f36bae71fd3f392d9
David Ernesto Ospina Piñeros <metrodeivjuegos@gmail.com>
2026-09-23 19:17:22 -0500

    fix: Arreglé el bug de la pantalla power ups
    Ahora, ingresar a power ups y salir luego de jugar es funcional

--- robvalo-break-out-3d/Scripts/menu_pausa.gd
+++ robvalo-break-out-3d/Scripts/menu_pausa.gd
@@ -48,6 +48,7 @@ func _cerrar_power_ups() -> void:
 		power_ups_instancia = null
 
 func _on_home_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/UI/menu_principal.tscn")
 
 func _on_resume_button_pressed() -> void:
--- robvalo-break-out-3d/Scripts/menu_principal.gd
+++ robvalo-break-out-3d/Scripts/menu_principal.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+func _ready() -> void:
+	get_tree().paused = false
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/nivel.tscn")
