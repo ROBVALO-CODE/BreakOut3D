1092c73d054afd50d9a9260af983c7a1eea275ec
David Ernesto Ospina Piñeros <metrodeivjuegos@gmail.com>
2026-09-23 19:32:15 -0500

    fix: Problema de audio
    Por la manipulación del código al intentar corregir la pantalla de power ups, se bugeo una parte que hacia que al salir del nivel, igual seguía escuchándose la música, ya quedó arreglado.

--- robvalo-break-out-3d/Scripts/menu_pausa.gd
+++ robvalo-break-out-3d/Scripts/menu_pausa.gd
@@ -49,6 +49,7 @@ func _cerrar_power_ups() -> void:
 
 func _on_home_button_pressed() -> void:
 	get_tree().paused = false
+	MusicPlayer.stop()
 	get_tree().change_scene_to_file("res://Scenes/UI/menu_principal.tscn")
 
 func _on_resume_button_pressed() -> void:
